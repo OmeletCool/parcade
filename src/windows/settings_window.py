c598525dd0e9ead05b3da1ba1b6d9a494ee31ede
мистер мастер <dolzheil230320106@gmail.com>
2025-12-14 21:19:17 +0300

    1

--- src/windows/settings_window.py
+++ src/windows/settings_window.py
@@ -1,6 +1,8 @@
 import arcade
 from pyglet.graphics import Batch
 from resources.languages import LANGUAGES
+from arcade.gui import *
+from src.settings import settings
 
 
 class SettingsMenuView(arcade.View):
@@ -9,7 +11,7 @@ class SettingsMenuView(arcade.View):
         super().__init__()
         self.window = window
         self.language = window.language
-        
+
         self.batch = Batch()
 
         self.texts = {
@@ -23,12 +25,15 @@ class SettingsMenuView(arcade.View):
         self.background_sprite_list = arcade.SpriteList()
         self.background_sprite = None
 
+        self.ui_manager = UIManager()
+        self.ui_manager.enable()
+
         # Текстовые объекты
         self.title_text = None
         self.return_text = None
         self.change_language_text = None
         self.language_text = None
-        
+
         # Все текстовые объекты будем хранить в списке
         self.text_objects = []
 
@@ -36,54 +41,98 @@ class SettingsMenuView(arcade.View):
 
         arcade.load_font('resources/fonts/montserrat.ttf')
 
+    def create_lang_dropdown(self):
+        center_x = self.window.width // 2
+        center_y = self.window.height // 2
+
+        offset_x = -120
+        offset_y = 80
+
+        pos_x = center_x + offset_x
+        pos_y = center_y + offset_y
+
+        options = [
+            LANGUAGES['language'][0],
+            LANGUAGES['language'][1],
+            LANGUAGES['language'][2]
+        ]
+
+        self.lang_dropdown = UIDropdown(
+            x=pos_x,
+            y=pos_y,
+            default=LANGUAGES['change_language'][self.language],
+            options=options,
+            width=200,
+            height=45,
+        )
+
+        self.ui_manager.add(self.lang_dropdown)
+
+        @self.lang_dropdown.event('on_change')
+        def on_language_change(event: UIOnChangeEvent):
+            if event.new_value == LANGUAGES['language'][0]:
+                settings.language = 0
+                self.window.language = 0
+                self.language = 0
+            elif event.new_value == LANGUAGES['language'][1]:
+                settings.language = 1
+                self.window.language = 1
+                self.language = 1
+            elif event.new_value == LANGUAGES['language'][2]:
+                settings.language = 2
+                self.window.language = 2
+                self.language = 2
+
     def setup(self):
         self.load_background()
 
         self.text_objects.clear()
-        
+
+        self.create_lang_dropdown()
+
         self.title_text = arcade.Text(
-            text=self.texts['title'], 
-            x=self.window.width // 1.4, 
+            text=self.texts['title'],
+            x=self.window.width // 1.4,
             y=self.window.height * 0.8,
-            color=arcade.color.BLACK, 
-            font_size=32, 
-            font_name='montserrat', 
+            color=arcade.color.BLACK,
+            font_size=32,
+            font_name='montserrat',
             anchor_x='center',
             batch=self.batch
         )
         self.text_objects.append(self.title_text)
-        
+
         self.return_text = arcade.Text(
-            text=self.texts['press_to_return'], 
-            x=self.window.width // 2, 
+            text=self.texts['press_to_return'],
+            x=self.window.width // 2,
             y=self.window.height * 0.02,
-            color=arcade.color.BLACK, 
-            font_size=18, 
-            font_name='montserrat', 
+            color=arcade.color.BLACK,
+            font_size=18,
+            font_name='montserrat',
             anchor_x='center',
             batch=self.batch
         )
         self.text_objects.append(self.return_text)
-        
+
         self.change_language_text = arcade.Text(
-            text=self.texts['change_language'], 
-            x=self.window.width // 4.1, 
+            text=self.texts['change_language'],
+            x=self.window.width // 4.1,
             y=self.window.height * 0.71,
-            color=arcade.color.BLACK, 
-            font_size=28, 
-            font_name='montserrat', 
+            color=arcade.color.BLACK,
+            font_size=28,
+            font_name='montserrat',
             anchor_x='center',
             batch=self.batch
         )
         self.text_objects.append(self.change_language_text)
-        
+
         self.music_text = arcade.Text(
-            text=self.texts['music'], 
-            x=self.window.width // 3.15, 
+            text=self.texts['music'],
+            x=self.window.width // 3.15,
             y=self.window.height * 0.51,
-            color=arcade.color.BLACK, 
-            font_size=18, 
-            font_name='montserrat', 
+            color=arcade.color.BLACK,
+            font_size=18,
+            font_name='montserrat',
             anchor_x='center',
             batch=self.batch
         )
@@ -102,6 +151,8 @@ class SettingsMenuView(arcade.View):
         # Все текстовые объекты отрисовываются через batch
         self.batch.draw()
 
+        self.ui_manager.draw()
+
     def on_key_press(self, symbol, modifiers):
         """Обработка нажатия клавиш"""
         # ESC для возврата в главное меню
@@ -113,6 +164,7 @@ class SettingsMenuView(arcade.View):
         super().on_resize(width, height)
         self.update_background_position_and_size()
         self.update_text_position()
+        self.ui_manager.on_resize(width, height)
 
     def load_background(self):
         """Загрузка фона"""
@@ -158,7 +210,7 @@ class SettingsMenuView(arcade.View):
         if self.return_text:
             self.return_text.x = self.window.width // 2
             self.return_text.y = self.window.height * 0.02
-            
+
         if self.change_language_text:
             self.change_language_text.x = self.window.width // 4.1
             self.change_language_text.y = self.window.height * 0.71
@@ -166,7 +218,3 @@ class SettingsMenuView(arcade.View):
         if self.music_text:
             self.change_language_text.x = self.window.width // 3.15
             self.change_language_text.y = self.window.height * 0.51
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.window.switch_view("main_menu")
